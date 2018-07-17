1a981c5be2144c7c70f52e95b1839b6c15278feb
johnvoyage <jmyork762@gmail.com>
2018-07-17 14:35:39 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'jy',
-    text: 'yup that work for me'
-  });
 });
 
 socket.on('disconnect', function () {
