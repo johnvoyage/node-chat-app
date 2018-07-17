225091a96403311c016d7f259a4d8593fcbd3362
johnvoyage <jmyork762@gmail.com>
2018-07-17 15:04:42 -0400

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,13 +14,32 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chate app',
+    createdAt: new Date().getTime(),
+  })
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime(),
+  })
+
   socket.on('createMessage', (message) => {
     console.log('createMessage:', message);
+
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // })
   });
 
   socket.on('disconnect', () => {
