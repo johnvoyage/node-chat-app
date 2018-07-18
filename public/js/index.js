098c1e7151c59910df6b9f9862c0bc74fc74a20f
johnvoyage <jmyork762@gmail.com>
2018-07-18 09:13:23 -0400

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,26 +29,31 @@ socket.on('newLocationMessage', function(message) {
 $('#message-form').on('submit', (event) => {
   event.preventDefault();
 
+  const messageTextbox = $('[name=message]')
+
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-    // console.log()
+    messageTextbox.val('');
   });
 });
 
 const locationButton = $('#send-location')
 locationButton.on('click', function (event) {
-  if (!navigator.geolocation) {
-    return alert('Geolocation not supported by your browser');
-  }
+  if (!navigator.geolocation) return alert('Geolocation not supported by your browser');
+
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
+
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location')
   });
 });
