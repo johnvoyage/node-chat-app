3790fc123224c8d6572d0ab03b2758f3dd757494
johnvoyage <jmyork762@gmail.com>
2018-07-18 13:13:04 -0400

    Add name to message

--- public/js/chat.js
+++ public/js/chat.js
@@ -75,7 +75,6 @@ $('#message-form').on('submit', (event) => {
   const messageTextbox = $('[name=message]')
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('');
